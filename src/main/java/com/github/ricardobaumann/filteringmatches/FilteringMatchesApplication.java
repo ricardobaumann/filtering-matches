4b235fe2f4993c1189d614eb64a657a871797125
ricardo.baumann <ricardo.baumann@outfittery.de>
2019-07-08 10:57:53 +0200

    Project setup

---
+++ src/main/java/com/github/ricardobaumann/filteringmatches/FilteringMatchesApplication.java
@@ -0,0 +1,13 @@
+package com.github.ricardobaumann.filteringmatches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilteringMatchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilteringMatchesApplication.class, args);
+	}
+
+}
