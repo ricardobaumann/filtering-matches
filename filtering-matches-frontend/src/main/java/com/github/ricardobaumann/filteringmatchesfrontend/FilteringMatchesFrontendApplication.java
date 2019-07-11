c01d1b8c04b8c3c3967db32dc7e418f344dd208a
ricardo.baumann <ricardo.baumann@outfittery.de>
2019-07-11 14:55:09 +0200

    frontend scaffold

---
+++ filtering-matches-frontend/src/main/java/com/github/ricardobaumann/filteringmatchesfrontend/FilteringMatchesFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.github.ricardobaumann.filteringmatchesfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilteringMatchesFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilteringMatchesFrontendApplication.class, args);
+	}
+
+}
---
+++ filtering-matches-frontend/src/test/java/com/github/ricardobaumann/filteringmatchesfrontend/FilteringMatchesFrontendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.ricardobaumann.filteringmatchesfrontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FilteringMatchesFrontendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
